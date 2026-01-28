90c51924ef689370c30006544109feffb101eb1f
Shipon Roy <shiponworks@gmail.com>
2026-01-28 18:49:14 +0600

    Completes

---
+++ Milestone 3 Hello Javascript/Module 16 Fundamentals of String & Objects/pratice task/script.js
@@ -0,0 +1,9 @@
+// 1 Output: // Output: joy joy (object is passed by reference, no copy is created)
+
+// 2 Output:// Output will be joy joy because objects are passed by reference, not copied.
+
+// 3 Output:// Output joy joy হবে কারণ object pass by reference হয়, এখানে কোনো copy তৈরি হয়নি।
+
+// 4 Output:// Output joy joy হবে কারণ object pass by reference হয়, এখানে কোনো copy তৈরি হয়নি।
+
+// 5 Output:// Both outputs are joy because obj1 and obj2 reference the same object in memory.
