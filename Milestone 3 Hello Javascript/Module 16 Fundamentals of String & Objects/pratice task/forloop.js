834228d87b9fb1318835c118aaa30129a5a46b8d
Shipon Roy <shiponworks@gmail.com>
2026-01-28 21:26:41 +0600

    loop larning live section

---
+++ Milestone 3 Hello Javascript/Module 16 Fundamentals of String & Objects/pratice task/forloop.js
@@ -0,0 +1,29 @@
+// For loop use
+
+for (let i = 0; i < 50; i++) {
+  console.log('love you baba and ma');
+}
+
+// Clear
+
+//*********** */ while loop ****
+
+// while (condition) {
+//   variable update
+// }
+
+let i = 0;
+while (i < 50) {
+  console.log('love you baba and ma');
+  i++;
+}
+
+//***** Do while Loop (optional)
+
+// age koro tarpor condition chack koro
+let n = 0;
+do {
+  console.log('love you baba and ma');
+} while (n < 100);
+
+//
