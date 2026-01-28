f95d6924f437421bf289dfcf5976e65745cfe36e
Shipon Roy <shiponworks@gmail.com>
2026-01-28 16:26:37 +0600

    Completes: oldenrodcolor access the color

---
+++ Milestone 3 Hello Javascript/Module 16 Fundamentals of String & Objects/pratice task/task.js
@@ -0,0 +1,34 @@
+// Object problems solving
+
+//? Qs: 1 : Access the golden rod color value in output.
+// Ans:
+const colors = {
+  red: '#ff0000',
+  green: '#00ff00',
+  blue: '#0000ff',
+  'golden rod': '#daa520',
+};
+
+const goldenRodColor = colors['golden rod'];
+console.log(goldenRodColor);
+
+//? Qs: 2 :
+// Ans:
+//? Qs: 3 :
+// Ans:
+//? Qs: 4 :
+// Ans:
+//? Qs: 5 :
+// Ans:
+//? Qs: 6 :
+// Ans:
+//? Qs: 7 :
+// Ans:
+//? Qs: 8 :
+// Ans:
+//? Qs: 9 :
+// Ans:
+//? Qs: 10 :
+// Ans:
+//? Qs: 11:
+// Ans:
