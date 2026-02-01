e9f1501d22060ab9a087d65bf237e9369fb838c8
Shipon Roy <shiponworks@gmail.com>
2026-02-01 22:22:16 +0600

    Completes: even number avg

--- Milestone 3 Hello Javascript/Module 18 JavaScript Simple Coding Problems (Part-1)/pratice/oddAvg.js
+++ Milestone 3 Hello Javascript/Module 18 JavaScript Simple Coding Problems (Part-1)/pratice/oddAvg.js
@@ -20,3 +20,27 @@ function oddAvg(numbers) {
 const numbers = [4, 2, 3, 5];
 const result = oddAvg(numbers);
 console.log(`Odd Average Numbers is: ${result}`);
+
+// Even Number Avg
+
+function evenAverage(numbers) {
+  let evens = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+      evens.push(numbers[i]);
+    }
+  }
+
+  let sum = 0;
+
+  for (let i = 0; i < evens.length; i++) {
+    sum += evens[i];
+  }
+
+  const average = sum / evens.length;
+  return average;
+}
+const evenNumber = [223, 546, 67, 78, 9, 3, 23];
+const resultEvenAvg = evenAverage(evenNumber);
+console.log(`Even Average Number: ${resultEvenAvg}`);
