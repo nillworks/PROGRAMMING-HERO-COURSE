a060ca3d8bfcf38d5014b7d81f40e4c798bdc4fb
Shipon Roy <shiponworks@gmail.com>
2026-02-02 08:40:44 +0600

    Completes: Date .js

---
+++ Milestone 3 Hello Javascript/Module 18 JavaScript Simple Coding Problems (Part-1)/pratice/date.js
@@ -0,0 +1,8 @@
+const today = new Date();
+const date = new Date();
+
+console.log(
+  `day is: ${date.getDay()} month is: ${today.getMonth()} Year is:${date.getFullYear()}`,
+);
+
+const specificDate = new Date();
