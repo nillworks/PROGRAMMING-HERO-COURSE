1ba4ef558a46ed963da6b196cfaffd1e952e3cc1
Shipon Roy <shiponworks@gmail.com>
2026-02-06 11:38:57 +0600

    completes: object methord revisions

---
+++ Milestone 3 Hello Javascript/Module 19 JavaScript Simple Coding Problems (Part 2)/revetion-javascript-all-problems-and-solve/objectMethord.js
@@ -0,0 +1,15 @@
+// Object Methods : Object.keys() Object.values() Object.entries()
+
+const priceQun = {
+  name: 'food',
+  price: 200,
+};
+
+// 1: সব key
+console.log(Object.keys(priceQun));
+
+// 2:সব value
+console.log(Object.values(priceQun));
+
+// 3:key+value
+console.log(Object.entries(priceQun));
