db62859c604f60685a56acb719aca386020e9308
Shipon Roy <shiponworks@gmail.com>
2026-02-14 06:40:57 +0600

    Compeltes: video 4

--- Milestone 04 Tentative Outline/Module 22 Tour of DOM (Document Object Model)/script.js
+++ Milestone 04 Tentative Outline/Module 22 Tour of DOM (Document Object Model)/script.js
@@ -5,3 +5,13 @@ const liItemsAll = document.getElementsByTagName('li');
 for (let i = 0; i < liItemsAll.length; i++) {
   console.log(liItemsAll[i].innerText);
 }
+
+const li = document.getElementById('li');
+const lis = document.getElementsByClassName('');
+const lisens = document.querySelector('');
+const lissen = document.querySelectorAll('');
+
+// innertext
+
+const liser = document.getElementsByClassName('inner');
+console.log(liser);
