a149ae912cfa4e78bbabad2e4ae5689b3d9c0588
Shipon Roy <shiponworks@gmail.com>
2026-03-12 06:07:13 +0600

    video 31-6 completes

--- Milestone 06/Module 31/31-2-Frist-projects/Header.jsx
+++ Milestone 06/Module 31/31-2-Frist-projects/Header.jsx
@@ -1,10 +1,20 @@
+const products = [
+  { id: 1, name: 'Laptop', price: 50000, brand: 'Dell' },
+  { id: 2, name: 'Mobile', price: 20000, brand: 'Samsung' },
+  { id: 3, name: 'Headphone', price: 2000, brand: 'Sony' },
+  { id: 4, name: 'Keyboard', price: 1500, brand: 'Logitech' },
+];
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <Parson products={products[0]} />
+    </div>
+  );
 };
 
-const Parson = () => {
-  const age = 17;
-  return <div>{age}</div>;
+const Parson = ({ name }) => {
+  return <div>{name}</div>;
 };
 
 export default Header;
