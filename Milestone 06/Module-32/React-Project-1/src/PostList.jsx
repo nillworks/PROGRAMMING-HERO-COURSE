dab9c8327acc4a7696e49071c404f853fc7f9473
Shipon Roy <shiponworks@gmail.com>
2026-03-14 18:35:09 +0600

    completes: vieo 8

--- Milestone 06/Module-32/React-Project-1/src/PostList.jsx
+++ Milestone 06/Module-32/React-Project-1/src/PostList.jsx
@@ -1,9 +1,18 @@
 import { use } from 'react';
+import PostRow from './PostRow';
 
 const PostList = ({ data }) => {
   const dataPost = use(data);
-  console.log(dataPost);
-  return <div>app</div>;
+  // console.log(dataPost);
+  return (
+    <div>
+      {dataPost.map(item => (
+        <li className="p-4 shadow space-y-1" key={item.id}>
+          {item.title}
+        </li>
+      ))}
+    </div>
+  );
 };
 
 export default PostList;
---
+++ Milestone 06/Module-32/React-Project-1/src/PostRow.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PostRow = () => {
+  return <div>PostRow</div>;
+};
+
+export default PostRow;
