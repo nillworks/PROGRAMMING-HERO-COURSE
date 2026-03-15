e1afedbdfb6d260401354f517e9df0efa6e2c0cc
Shipon Roy <shiponworks@gmail.com>
2026-03-15 07:28:58 +0600

    modifi projects

--- Milestone 06/Module-33/On-Tha-Go/src/Components/Countries.jsx
+++ Milestone 06/Module-33/On-Tha-Go/src/Components/Countries.jsx
@@ -6,7 +6,7 @@ const Countries = ({ allData }) => {
   const countries = useAllData.countries;
   console.log(countries);
   return (
-    <div className="grid justify-center grid-cols-1 items-center sm:grid-cols-2 lg:grid-cols-3">
+    <div className="gap-3 grid justify-center grid-cols-1 align-middle sm:grid-cols-2 lg:grid-cols-3 items-stretch">
       {countries.map(data => (
         <CountriesDetails key={data.cca3.cca3} countriesData={data} />
       ))}
--- Milestone 06/Module-33/On-Tha-Go/src/Components/Page/CountriesDetails.jsx
+++ Milestone 06/Module-33/On-Tha-Go/src/Components/Page/CountriesDetails.jsx
@@ -1,17 +1,39 @@
+import { useState } from 'react';
+
 const CountriesDetails = ({ countriesData }) => {
+  const [visited, setVisited] = useState(true);
+
+  const handleVisited = () => {
+    setVisited(prev => !prev);
+  };
+
   return (
-    <div className="text-white">
-      <img
-        src={countriesData?.flags?.flags?.png}
-        alt={countriesData?.flags?.flags?.alt}
-      />
-      <h2 className="text-2xl">
-        CountriesName:{'  '}
-        <span className="font-bold text-lg capitalize">
-          {countriesData?.name?.common}
-        </span>
-      </h2>
-      <p>Population : {countriesData?.population?.population}</p>
+    <div className="text-white border mx-auto px-2 py-4 rounded-2xl transition duration-300 cursor-pointer hover:-translate-y-1">
+      {/* Details */}
+      <div className="space-y-3 text-center">
+        <img
+          src={countriesData?.flags?.flags?.png}
+          alt={countriesData?.flags?.flags?.alt}
+        />
+        <h2 className="text-2xl">
+          CountriesName:{'  '}
+          <span className="font-bold text-lg capitalize">
+            {countriesData?.name?.common}
+          </span>
+        </h2>
+        <p>Population : {countriesData?.population?.population}</p>
+        <p>
+          Area:{' '}
+          {countriesData.area.area > 10000
+            ? `Beg Countries ${countriesData.area.area}`
+            : countriesData.area.area}
+        </p>
+        <div>
+          <button onClick={handleVisited} className="btn btn-primary mx-auto">
+            {visited ? 'Not Visited' : 'Visited'}
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
