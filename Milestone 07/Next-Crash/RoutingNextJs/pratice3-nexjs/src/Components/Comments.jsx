9b3fcc7e4b0bff29303dcdd82ddfebd34bff1d71
Shipon Roy <shiponworks@gmail.com>
2026-04-09 21:37:01 +0600

    complete: larning and pratice

--- Milestone 07/Next-Crash/RoutingNextJs/pratice3-nexjs/src/Components/Comments.jsx
+++ Milestone 07/Next-Crash/RoutingNextJs/pratice3-nexjs/src/Components/Comments.jsx
@@ -2,11 +2,19 @@ const Comments = async ({ promise }) => {
   const { name, body } = await promise;
 
   return (
-    <div className="space-y-1">
-      <h2 className="text-lg font-bold">{name}</h2>
-      <h2 className="text-lg">{body}</h2>
+    <div className="bg-white shadow-md rounded-2xl p-5 border border-gray-200 hover:shadow-lg transition duration-300">
+      <h2 className="text-xl font-semibold text-blue-600 mb-2">{name}</h2>
+
+      <p className="text-gray-600 leading-relaxed">{body}</p>
     </div>
   );
 };
 
 export default Comments;
+
+//1. CSR == Client side Rendering Or (CSR).
+//2. SSR == Server Side Rendering or (SSR).
+
+//3. Static Side Generation (SSG)
+//4. incremental Static Regeneration (ISR)
+//5. Partial Prerendering (PPR)
